b4a4599cb041d6dfb70e70fcb1bf959bbc4fc50c
Joshua Williams <jmw0426@hotmail.com>
2020-06-12 20:43:59 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,9 +7,8 @@ require "pry"
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
+# location: project.css("ul.project-meta").text
 
-# project.css("ul.project-meta").text
-#.li.a data-location"
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   
